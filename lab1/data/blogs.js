b553921252f69510a8396ee263bf9a53defd469c
Krystal732 <krystalhong732@gmail.com>
2024-01-28 17:49:48 -0500

    first

--- lab1/data/blogs.js
+++ lab1/data/blogs.js
@@ -16,9 +16,19 @@ import {ObjectId} from 'mongodb';
 
 
 export const showBlogs = async(skip = 0, limit = 20) =>{
+  if(typeof skip !== 'number' || typeof limit !== 'number'){
+    throw `skip and limit must be type number`
+  }
+  if(!Number.isInteger(skip) || !Number.isInteger(limit)){
+    throw `skip and limit must be integers`
+  }
+  if(skip < 0 || limit < 0){
+    throw 'Skip and limit must be non-negative numbers'
+  }
   if (limit > 100){ //limit 100
     limit = 100
   }
+  
   const blogsCollection = await blogs()
   const blogList = await blogsCollection.find().skip(skip).limit(limit).toArray();
   return blogList
@@ -56,38 +66,182 @@ export const createBlog = async (
   const blogsCollection = await blogs()
   const insertInfo = await blogsCollection.insertOne(newBlog);
 	if (!insertInfo.acknowledged || !insertInfo.insertedId){
-    throw 'Could not add review';
+    throw 'Could not add blog';
   }
   const newId = insertInfo.insertedId.toString();
-  const blog = await reviewCollection.findOne({_id: new ObjectId(newId)});
+  const blog = await blogsCollection.findOne({_id: new ObjectId(newId)});
   return blog
 };
 
-export const putBlog = async () =>{
+export const putBlog = async (newBlogTitle, newBlogBody, blogId) =>{
+  newBlogTitle = checkAndTrimString(newBlogTitle)
+  newBlogBody = checkAndTrimString(newBlogBody)
+  blogId = checkAndTrimString(req.params.id, 'blogId');
+  if (!ObjectId.isValid(blogId)) throw `invalid object ID`;
+
+  const newBlog = {
+    blogTitle: newBlogTitle,
+    blogBody: newBlogBody
+  }
+
+  const blogsCollection = await blogs()
+  const updatedInfo = await blogsCollection.findOneAndUpdate(
+    {_id: new ObjectId(blogId)},
+    {$set: newBlog},
+    {returnDocument: 'after'}
+  );
+  
+  if (!updatedInfo) {
+    throw `could not update blog successfully`;
+  }
+  updatedInfo._id = updatedInfo._id.toString();
+  return updatedInfo;
 
 };
 
-export const patchBlog = async() =>{
+export const patchBlog = async(newBlogTitle, newBlogBody, blogId) =>{
+  blogId = checkAndTrimString(req.params.id, 'blogId');
+  if (!ObjectId.isValid(blogId)) throw `invalid object ID`;
+
+  const newBlog = {}
+  if (newBlogTitle !== undefined) {
+    newBlogTitle = checkAndTrimString(newBlogTitle)
+    newBlog.blogTitle = newBlogTitle
+  }
+  if (newBlogBody !== undefined) {
+    newBlogBody = checkAndTrimString(newBlogBody)
+    newBlog.blogBody = newBlogBody
+  }
+
+  const blogsCollection = await blogs()
+  const updatedInfo = await blogsCollection.findOneAndUpdate(
+    {_id: new ObjectId(blogId)},
+    {$set: newBlog},
+    {returnDocument: 'after'}
+  );
+  
+  if (!updatedInfo) {
+    throw `could not update blog successfully`;
+  }
+  updatedInfo._id = updatedInfo._id.toString();
+  return updatedInfo;
 
 };
 
-export const postComment = async(id) =>{
+export const postComment = async(blogId, 
+  comment, 
+  userID, 
+  username) =>{
+  blogId = checkAndTrimString(req.params.id, 'blogId');
+  if (!ObjectId.isValid(blogId)) throw `blogId is invalid object ID`;
 
+  comment = checkAndTrimString(comment)
+  username = checkAndTrimString(username)
+
+  if (!(userID instanceof ObjectId)) {
+    throw `userID must be ObjectId`;
+  }
+
+  const newComment = {
+    _id: new ObjectId(),
+    userThatPostedComment: {_id:userID, username: string},
+    comment: comment
+  }
+
+  const blogsCollection = await blogs()
+  const updatedInfo = await blogsCollection.findOneAndUpdate(
+    {_id: new ObjectId(blogId)},
+    {$push: {comments: newComment}},
+    {returnDocument: 'after'}
+  );
+  
+  if (!updatedInfo) {
+    throw `could not update blog successfully`;
+  }
+  updatedInfo._id = updatedInfo._id.toString();
+  return updatedInfo;
+  
 };
 
-export const deleteComment = async(blogId) => {
+export const deleteComment = async(blogId, commentId) => {
+  blogId = checkAndTrimString(req.params.id, 'blogId');
+  if (!ObjectId.isValid(blogId)) throw `blogId is invalid object ID`;
 
+  commentId = checkAndTrimString(req.params.id, 'commentId');
+  if (!ObjectId.isValid(commentId)) throw `commentId is invalid object ID`;
+
+  const blogsCollection = await blogs()
+
+  const updatedInfo = await blogsCollection.findOneAndUpdate(
+    { _id: new ObjectId(blogId) },
+    { $pull: { comments: { _id: commentId } } },
+    { returnDocument: 'after' }
+  );
+
+  if (!updatedInfo) {
+    throw `Could not find the blog with ID ${blogId}`
+  }
+
+  updatedInfo._id = updatedInfo._id.toString()
+  return updatedInfo
 };
 
 
 export const registerUser = async (
+  name,
   username,
   password
 ) => {
+  name = checkAndTrimString(name, "name")
+  username = checkAndTrimString(username, "username")
+  password = checkAndTrimString(password, "password")
 
+  let saltRounds = 16
+  const hash = await bcrypt.hash(password, saltRounds);
+
+  const newUser = {
+    name: name,
+    username: username,
+    password: hash
+  }
+  
+  const userCollection = await users()
+  const insertInfo = await userCollection.insertOne(newUser);
+	if (!insertInfo.acknowledged || !insertInfo.insertedId){
+    throw 'Could not add user';
+  }
+  const newId = insertInfo.insertedId.toString();
+  const user = await userCollection.findOne({_id: new ObjectId(newId)});
+  const userSansPassword = {
+    _id: user._id,
+    name: user.name,
+    username: user.username,
+  };
+  
+  return userSansPassword;
 };
 
 export const signinUser = async (username, password) => {
+  username = checkAndTrimString(username)
+  password = checkAndTrimString(password)
+  //find username
+  const userCollection = await users()
+  const user = await userCollection.findOne({username: username});
+  if(user === null){
+    throw `Either the email address or password is invalid`
+  }
 
+  //check hashed passowrd
+  if(!(await bcrypt.compare(password, user.password))){
+    throw `Either the email address or password is invalid`
+  }
+
+  const userSansPassword = {
+    _id: user._id,
+    name: user.name,
+    username: user.username,
+  };
+  
+  return userSansPassword;
 };
 
--- lab1/routes/blog_routes.js
+++ lab1/routes/blog_routes.js
@@ -9,18 +9,37 @@ router
   .route('/sitblog')
   .get(async (req, res) => {
     try{
-      const skip = parseInt(req.query.skip) || 0
-      const limit = parseInt(req.query.limit) || 20
+      let skip = undefined
+      let limit = undefined
+      if(req.query.skip){
+        if(/^\d+$/.test(req.query.skip)){//if string rep of int
+          skip = parseInt(req.query.skip)
+        }else{
+          throw `skip must be a non-negative int`
+        }
+      }else{
+        skip = 0
+      }
+      if(req.query.limit){
+        if(/^\d+$/.test(req.query.limit)){//if string rep of int
+          limit = parseInt(req.query.limit)
+        }else{
+          throw `limit must be a non-negative int`
+        }
+      }else{
+        limit = 20
+      }
 
       //limit 100
       if (limit > 100) {
         limit = 100
       }
+      
       const blogList = await showBlogs(skip, limit);
-      res.status(200).json(blogList);
+      return res.status(200).json(blogList);
 
     }catch(e){
-      res.status(500).json({error: e});
+      return res.status(400).json({error: e});
     }
   })
   .post(async (req, res) => {
@@ -75,27 +94,215 @@ router
 
   })
   .put(async (req, res) => {
+    let updates = req.body
+    let blogId = req.params.id
+    let user = undefined
+    try{ 
+      //check req.body
+      if(!updates.blogTitle || !updates.blogBody){
+        throw `Missing parameters`
+      }
+      updates.blogTitle = checkAndTrimString(updates.blogTitle, "updated blog title")
+      updates.blogBody = checkAndTrimString(updates.blogBody, "updated blog body")
+
+      //check id
+      blogId = checkAndTrimString(req.params.id, 'blogId');
+      if (!ObjectId.isValid(blogId)) throw `invalid object ID`;
+
+      //check user
+      user = req.session.user
+      if (!user){
+        return res.status(401).json({error: 'Must be logged in to update a blog'});
+      }
+      let blog = await getBlog(blogId)
+      if (user._id !== blog.userThatPosted._id || user.username !== blog.userThatPosted.username){
+        return res.status(403).json({error: 'Must be logged in the correct account to update'})
+      }
+    }catch(e){
+      return res.status(400).json({error:e})
+    }
+    try{
+      let complete = await putBlog(
+        updates.blogTitle,
+        updates.blogBody,
+        blogId
+      )
+      if (complete){
+        res.status(200).json({complete})
+      }
+      else{
+        res.status(500).json({error: "Internal Server Error"})
+      }
+    }catch(e){
+      res.status(400).json({error:e})
+    }
     
   })
   .patch(async (req, res) => {
+    let updates = req.body
+    let blogId = req.params.id
+    let user = undefined
+    try{ 
+      //check req.body
+      if(updates.blogTitle){
+        updates.blogTitle = checkAndTrimString(updates.blogTitle, "updated blog title")
+      }
+      if(updates.blogBody){
+        updates.blogBody = checkAndTrimString(updates.blogBody, "updated blog body")
+      }
+      //check id
+      blogId = checkAndTrimString(req.params.id, 'blogId');
+      if (!ObjectId.isValid(blogId)) throw `invalid object ID`;
+
+      //check user
+      user = req.session.user
+      if (!user){
+        return res.status(401).json({error: 'Must be logged in to update a blog'});
+      }
+      let blog = await getBlog(blogId)
+      if (user._id !== blog.userThatPosted._id || user.username !== blog.userThatPosted.username){
+        return res.status(403).json({error: 'Must be logged in the correct account to update'})
+      }
+    }catch(e){
+      return res.status(400).json({error:e})
+    }
+    try{
+      let complete = await putBlog(
+        updates.blogTitle,
+        updates.blogBody,
+        blogId
+      )
+      if (complete){
+        res.status(200).json({complete})
+      }
+      else{
+        res.status(500).json({error: "Internal Server Error"})
+      }
+    }catch(e){
+      res.status(400).json({error:e})
+    }
     
   });
 
 
 router.route('/sitblog/:id/comments').post(async (req, res) => {
+  let comment = req.body
+  let user = undefined
+  let blogId = req.params.id
+
+  try{
+    comment = checkAndTrimString(comment)
+
+    blogId = checkAndTrimString(req.params.id, 'blogId');
+    if (!ObjectId.isValid(blogId)) throw `invalid object ID`;
+
+    user = req.session.user
+    if (!user){
+      return res.status(401).json({error: 'Must be logged in to comment on blog'});
+    }      
+
+    let complete = await postComment(
+      blogId, 
+      comment, 
+      user._id,
+      user.username
+    )
+    if (complete){
+      res.status(200).json({complete})
+    }
+    else{
+      res.status(500).json({error: "Internal Server Error"})
+    }
+  }catch(e){
+    res.status(400).json({error:e})
+  }
   
 });
 
 router.route('/sitblog/:blogId/:commentId').delete(async (req, res) => {
+  let user = undefined
+  let blogId = req.params.blogId
+  let commentId = req.params.commentId
+
+  try{
+    commentId = checkAndTrimString(commentId)
+    if (!ObjectId.isValid(commentId)) throw `commentId is invalid object ID`;
+
+
+    blogId = checkAndTrimString(req.params.id, 'blogId');
+    if (!ObjectId.isValid(blogId)) throw `blogId is invalid object ID`;
+
+    user = req.session.user
+    if (!user){
+      return res.status(401).json({error: 'Must be logged in to comment on blog'});
+    }
+    
+    let blog = await getBlog(blogId)
+    let matchingComment = blog.comments.find(comment => comment._id === commentId);
+    if(!matchingComment){
+      throw `Comment does not exist`
+    }
+    if(matchingComment.userThatPostedComment._id !== user._id || matchingComment.userThatPostedComment.username !== user.username){
+      return res.status(403).json({error: 'Must be logged in the correct account to delete comment'})
+    }
+
+    let complete = await deleteComment(
+      blogId, 
+      commentId
+    )
+    if (complete){
+      res.status(200).json({complete})
+    }
+    else{
+      res.status(500).json({error: "Internal Server Error"})
+    }
+  }catch(e){
+    res.status(400).json({error:e})
+  }
   
 });
 
 router.route('/sitblog/register').post(async (req, res) => {
-  
+  let user = req.body
+  try{
+    user.name = checkAndTrimString(user.name, "user's name")
+    user.username = checkAndTrimString(user.username, "username")
+    user.password = checkAndTrimString(user.password, "password")
+    let complete = await registerUser(
+      user.name,  
+      user.username, 
+      user.password
+    )
+    if (complete){
+      res.status(200).json({complete})
+    }
+    else{
+      res.status(500).json({error: "Internal Server Error"})
+    }
+  }catch(e){
+    res.status(400).json({error:e})
+  }
 });
 
 router.route('/sitblog/signin').post(async (req, res) => {
-  
+  let user = req.body
+  try{
+    user.username = checkAndTrimString(user.username, "username")
+    user.password = checkAndTrimString(user.password, "password")
+    let complete = await signinUser(
+      user.username, 
+      user.password
+    )
+    if (complete){
+      req.session.user = complete
+      res.status(200).json({complete})
+    }
+    else{
+      res.status(500).json({error: "Internal Server Error"})
+    }
+  }catch(e){
+    res.status(400).json({error:e})
+  }
 });
 
 router.route('/sitblog/logout').get(async (req, res) => {
